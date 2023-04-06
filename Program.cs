092b438c8fcd42d8e588d928b3db8085cf666ad4
MartinL-no <martin.lorimer@icloud.com>
2023-04-06 17:46:55 +0200

    initialise fork

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ using System.Threading;
 
 namespace ConsoleStars
 {
+    /*
+        The program shows two types of stars, one that flashes and one that moves.
+        Rewrite the code by introducing an interface, so that the foreloop in the
+        main program can be simplified to this: 
+
+        foreach (var star in stars)
+        {
+            star.Show();
+            star.Update();
+        }
+     */
     class Program
     {      
         static void Main(string[] args)
